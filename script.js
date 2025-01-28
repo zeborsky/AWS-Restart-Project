f73cc39e97653fbdecc5868394d1b1275597c072
joshkodjo <josh12cod3@yahoo.com>
2025-01-28 22:17:27 +0000

    update

--- script.js
+++ script.js
@@ -54,3 +54,11 @@ document.getElementById('videoModal').addEventListener('click', function (e) {
         closeVideoModal();
     }
 });
+
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').slice(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
